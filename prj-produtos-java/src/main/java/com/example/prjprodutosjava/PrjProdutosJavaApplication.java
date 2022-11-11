4bae9472dac110ca0caa2c9840c3ba6386b2e7df
Fernando da Rocha Moreira <fernando.r.moreira@aluno.senai.br>
2022-11-11 20:41:32 -0300

    commit zero

---
+++ prj-produtos-java/src/main/java/com/example/prjprodutosjava/PrjProdutosJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.prjprodutosjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjProdutosJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjProdutosJavaApplication.class, args);
+	}
+
+}
---
+++ prj-produtos-java/src/test/java/com/example/prjprodutosjava/PrjProdutosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prjprodutosjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjProdutosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
