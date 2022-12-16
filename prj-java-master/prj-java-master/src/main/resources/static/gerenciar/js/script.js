675f71a704f12a4ba62f25d63f587a85e4389636
Fernando da Rocha Moreira <fernando.r.moreira@aluno.senai.br>
2022-12-16 12:01:40 -0300

    corrigindo problemas

--- prj-java-master/prj-java-master/src/main/resources/static/gerenciar/js/script.js
+++ prj-java-master/prj-java-master/src/main/resources/static/gerenciar/js/script.js
@@ -2,7 +2,7 @@
 // 01 criar a váriavel
 xhttp = new XMLHttpRequest();
 var lista;
-var api = "https://prj-produtos-java.herokuapp.com/api/usuario/";
+var api = "http://localhost:8080/api/produto/";
 
 function listar() {
     // 02 definição do nosso request (forma e endereço)
@@ -18,7 +18,7 @@ function listar() {
         texto = "";
         i = 0;
         for (const u of lista) {
-            texto += `<tr onclick='editar(${i})'><td>${u.nome}</td><td>${u.email}</td></tr>`;
+            texto += `<tr onclick='editar(${i})'><td>${u.nome}</td><td>R$ ${u.valor.toFixed(2)}</td></tr>`;
             i++;
         }
         document.getElementById('lista').innerHTML = texto;
@@ -28,18 +28,23 @@ function listar() {
 function editar(i) {
     u = lista[i];
     document.getElementById("nome").value = u.nome;
-    document.getElementById("email").value = u.email;
+    document.getElementById("descricao").value = u.descricao;
+    document.getElementById("valor").value = u.valor;
+    document.getElementById("img").value = u.img;
     document.getElementById("id").value = u.id;
 }
 
 function gravar() {
     //alert("Estamos dentro da function incluir");
-    var usuario = {};
-    usuario.nome = document.getElementById("nome").value;
-    // console.log(usuario);
+    var item = {};
+    item.nome = document.getElementById("nome").value;
+    item.descricao = document.getElementById("descricao").value;
+    item.valor = document.getElementById("valor").value;
+    item.img = document.getElementById("img").value;
+    // console.log(item);
 
-    usuario.id = document.getElementById("id").value;
-    if (usuario.id > 0) {
+    item.id = document.getElementById("id").value;
+    if (item.id > 0) {
         acao = "PUT"; // alteração
     } else {
         acao = "POST"; // incluir
@@ -47,7 +52,7 @@ function gravar() {
 
     xhttp.open(acao, api);
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(JSON.stringify(usuario));
+    xhttp.send(JSON.stringify(item));
     xhttp.onload = function () {
         // console.log(this.responseText);
         listar();
@@ -57,7 +62,9 @@ function gravar() {
 
 function limpar() {
     document.getElementById("nome").value = "";
-    document.getElementById("email").value = "";
+    document.getElementById("descricao").value = "";
+    document.getElementById("valor").value = "";
+    document.getElementById("img").value = "";
     document.getElementById("id").value = "";
 }
 
